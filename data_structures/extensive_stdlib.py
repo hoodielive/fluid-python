bd53df33b177c9aa99452c466709ceb04410c6f8
hoodielive <hoodielive@gmail.com>
2019-01-27 12:34:38 -0500

    explore pystdlib

---
+++ data_structures/extensive_stdlib.py
@@ -0,0 +1,13 @@
+# Python Standard Library
+# collections
+# heapq
+# pickle - python object serialization
+# sqlite3
+# json
+# base64
+# html/xml
+# urllib - (but I'm moving to requests in everything)
+# itertools - for efficeient looping
+# enum - support enumerations (named enumerations)
+# pathlib - object oriented filesystem paths
+# inspect - get information about data objects
