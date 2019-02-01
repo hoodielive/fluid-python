4e81da810aaf3ee3319aa6e5c60f55467ac6daee
hoodielive <hoodielive@gmail.com>
2019-02-01 13:05:15 -0500

    expand it a bit

--- stuff/enumerate_an_iterator.py
+++ stuff/enumerate_an_iterator.py
@@ -2,3 +2,8 @@ pets = ['dogs', 'cats', 'lizards', 'pythons']
 pet_enum = enumerate(pets)
 print(next(pet_enum))
 
+pet_enum2 = enumerate(pets, start=5)
+print(next(pet_enum2))
+
+pet_list = list(enumerate(pets))
+print(pet_list)
