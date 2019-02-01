4cf6aa0e9e049c70bda1eaf8dbe8548ef4a71908
hoodielive <hoodielive@gmail.com>
2019-02-01 17:42:59 -0500

    private_class

---
+++ stuff/private_class.py
@@ -0,0 +1,7 @@
+class MyComplex:
+    def __init__(self, real_part, imaginary_part):
+        self.r = real_part
+        self.i = imaginary_part
+
+complex_num = MyComplex(2, 3)
+print(complex_num)
