fdd18e84a7cf66ed763e3a60d818f5abe0cd7b5d
hoodielive <hoodielive@gmail.com>
2019-02-01 17:43:32 -0500

    private_class

--- stuff/private_class.py
+++ stuff/private_class.py
@@ -3,5 +3,8 @@ class MyComplex:
         self.r = real_part
         self.i = imaginary_part
 
+class PrivateClass:
+    __a_ = 'I am a private class'
+
 complex_num = MyComplex(2, 3)
 print(complex_num)
